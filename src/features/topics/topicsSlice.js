e231553ed791387a1f150180db8de11dc3bb7155
Emin Koroglu <eminnkoroglu.1@gmail.com>
2022-01-21 12:45:01 -0600

    bugfixing

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,15 +1,13 @@
-import {createSlice, createStore} from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
 
-const topics = createSlice({
+const topicsSlice = createSlice({
     name: 'topics',
     initialState: {
-        topics: {
-
-        },
+        topics: {},
     },
     reducers: {
         addTopic: (state, action) => {
-            const {id, name, icon} = action.payload;
+            const { id, name, icon } = action.payload;
             state.topics[id] = {
                 id: id,
                 name: name,
@@ -18,14 +16,13 @@ const topics = createSlice({
             };
         },
         addQuizId: (state, action) => {
-            const {quizId, topicId} = action.payload;
+            const { quizId, topicId } = action.payload;
             state.topics[topicId].quizIds.push(quizId);
         },
     }
 });
 
 //exports
-export const topicsSlice = createSlice(topics);
-export const selectTopics = (state) => state.topics.topics;
-export const {addTopic, addQuizId} = topicsSlice.actions;
+export const selectTopics = (state) => state.topics;
+export const { addTopic, addQuizId } = topicsSlice.actions;
 export default topicsSlice.reducer;
