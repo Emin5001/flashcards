09e07c2597b774589b261aaaf8720c95591082a8
Emin Koroglu <eminnkoroglu.1@gmail.com>
2022-01-21 13:00:47 -0600

    fixed topic creation

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -23,6 +23,6 @@ const topicsSlice = createSlice({
 });
 
 //exports
-export const selectTopics = (state) => state.topics;
+export const selectTopics = (state) => state.topics.topics;
 export const { addTopic, addQuizId } = topicsSlice.actions;
 export default topicsSlice.reducer;
